49f242c040167d0ca523d930011f3cb8b6acdc68
Wenying Chang <changwenyingnl@gmail.com>
2024-05-10 15:21:13 +0200

    install MUI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import Navbar from './navbar/navbar';
-import Section from './section/section';
+import Section1 from './prevelance_around_the_world/section1';
 import './App.css';
 
 
@@ -17,10 +17,10 @@ function App() {
     <div className="App">
       <Navbar onNavbarClick={handleNavbarClcik}/>
       <div className='sections'>
-        <Section id="section1" isActive={activeSelection === "section1"}/>
-        <Section id="section2" isActive={activeSelection === "section2"}/>
-        <Section id="section3" isActive={activeSelection === "section3"}/>
-        <Section id="section4" isActive={activeSelection === "section4"}/>
+        <Section1 id="section1" isActive={activeSelection === "section1"}/>
+        <Section1 id="section2" isActive={activeSelection === "section2"}/>
+        <Section1 id="section3" isActive={activeSelection === "section3"}/>
+        <Section1 id="section4" isActive={activeSelection === "section4"}/>
 
       </div>
      

5e3abafe76c593eed4e01d889bd07b67359bb421
Wenying Chang <changwenyingnl@gmail.com>
2024-07-24 10:48:43 +0200

    fix:/slider styles

--- src/slider/YearSlider.js
+++ src/slider/YearSlider.js
@@ -1,6 +1,40 @@
 import React from "react";
 import "./slider.css";
-import { Box, Slider} from '@mui/material';
+import { Box, Slider, styled } from '@mui/material';
+
+const CustomSlider = styled(Slider)({
+    color: "#f50057", // Selected track color
+    '& .MuiSlider-thumb': {
+      height: 24,
+      width: 24,
+      backgroundColor: '#fff',
+      border: '2px solid rgba(71, 88, 220, 1)',
+      boxShadow: '1 1 0 4px rgba(0, 0, 0, 0.25)',
+      '&:hover': {
+        boxShadow: '0 0 0 8px rgba(71, 88, 220, 0.2)', // Thumb hover effect
+      },
+    },
+    '& .MuiSlider-track': {
+        color: '#4758DC',
+      height: 8,
+      borderRadius: 4,
+    },
+    '& .MuiSlider-rail': {
+      color: '#4758DC',
+      height: 8,
+      borderRadius: 4,
+    },
+    '& .MuiSlider-valueLabel': {
+      left: 'calc(-50% px)',
+      top: -20,
+      backgroundColor: '#EFEFEF',
+      boxShadow: '2 2 0 4px rgba(0, 0, 0, 0.25)',
+      '& *': {
+        background: 'transparent',
+        color: '#000',
+      },
+    },
+});
 
 const YearSlider = ({year, handleYearChange}) => {
     
@@ -13,7 +47,7 @@ const YearSlider = ({year, handleYearChange}) => {
         <>
             <div className="slider-label">1990</div>
                 <Box sx={{ width: 600 }}>
-                    <Slider 
+                    <CustomSlider
                         track={false}
                         value={year} 
                         onChange={handleYearChange}
@@ -21,7 +55,7 @@ const YearSlider = ({year, handleYearChange}) => {
                         getAriaValueText={valueText}
                         min={1990} 
                         max={2019}
-                        color="primary"
+                        color="secondary"
                     />
                 </Box>
             <div className="slider-label">2019</div>
